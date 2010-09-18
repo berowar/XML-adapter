d27b387981fd5ce4a5e40a5b0fa2257cc5ec782a
Dmitry Puchkov <berowar@gmail.com>
2010-09-18 11:05:08 +0300

    add tests

--- test/translate_test.rb
+++ test/translate_test.rb
@@ -18,7 +18,14 @@ describe Localize do
     t = Localize.translate
     t.hello.should == 'world'
   end
-
+  
+  it "Get translation from xml" do
+    Localize.store = :xml
+    Localize.load(:en, 'stores')
+    t = Localize.translate
+    t.hello.should == 'world'
+  end
+  
   it "Accept nested translations" do
     t = Localize.translate
     t.foo.bar.should == 'baz'
---
+++ test/xml_formats_test.rb
@@ -0,0 +1,24 @@
+require File.join(File.dirname(__FILE__), '../lib/localize')
+
+describe Localize do
+  before do
+    Localize.store = :xml
+    Localize.location = 'stores'
+  end
+
+  it "Format phone numbers" do
+    Localize.f(380446666666, :full).should == '+380 (44) 666-66-66'
+  end
+
+  it "Localize dates" do
+    Localize.l(Time.utc(2000, "jan")).should == 'Sat Jan 01 00:00:00 UTC 2000'
+  end
+
+  it "Format dates" do
+    Localize.l(Time.utc(2000, "jan"), :short).should == 'Sat 01-January-00'
+  end
+
+  it "Format numbers" do
+    Localize.l(1000.02).should == '1,000.02'
+  end
+end
