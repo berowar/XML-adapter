b4f5c1d82c6e972a6ea2d4d4646dfb7a8cbad09a
Dmitry Puchkov <berowar@gmail.com>
2010-09-18 16:29:08 +0300

    some change xml.rb

--- lib/localize/adapters/xml.rb
+++ lib/localize/adapters/xml.rb
@@ -5,10 +5,9 @@ module Localize
     def self.get_trans
       tr = {}
       Dir.glob(File.join(Localize::location, "#{Localize::locale.to_s}.xml")).each do |file|
-        f = (RUBY_VERSION < '1.9') ? ::File.open(file) : ::File.open(file, 'r:utf-8')
-        tr.merge! ::XmlSimple.xml_in( f, 'KeyAttr'    => { 'item' => 'name' },
-                                         'ForceArray' => [ 'item' ],
-                                         'ContentKey' => '-content')
+        tr.merge! ::XmlSimple.xml_in(file, 'KeyAttr'    => { 'item' => 'name' },
+                                           'ForceArray' => [ 'item' ],
+                                           'ContentKey' => '-content')
       end
       tr
     end
